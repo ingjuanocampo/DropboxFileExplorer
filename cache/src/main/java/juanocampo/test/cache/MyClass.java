d057692ee0b00503d4e47084e0aa4a33bda0ca66
juan ocampo <juan.ocampo@globant.com>
2019-10-10 23:24:52 -0500

    Added Cache and FileExplorerSdk Module

---
+++ cache/src/main/java/juanocampo/test/cache/MyClass.java
@@ -0,0 +1,4 @@
+package juanocampo.test.cache;
+
+public class MyClass {
+}
--- file_explorer_sdk/src/androidTest/java/juanocampo/test/file_explorer_sdk/ExampleInstrumentedTest.kt
+++ file_explorer_sdk/src/androidTest/java/juanocampo/test/file_explorer_sdk/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package juanocampo.test.dropboxexplorer
+package juanocampo.test.file_explorer_sdk
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("juanocampo.test.dropboxexplorer", appContext.packageName)
+        assertEquals("juanocampo.test.file_explorer_sdk.test", appContext.packageName)
     }
 }
