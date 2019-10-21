f6dc136b3ba504abff25b988e3bf1ca382f53392
juan manuel Ocampo valencia <>
2019-10-21 08:50:13 -0500

    Kotlin DSL migration first step

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+
+import org.gradle.kotlin.dsl.extra
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.setValue
+import org.gradle.kotlin.dsl.repositories
+
+plugins {
+    `kotlin-dsl`
+}
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+
+    }
+}
